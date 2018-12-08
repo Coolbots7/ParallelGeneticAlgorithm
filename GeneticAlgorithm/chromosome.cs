8191e365d12bc89553806f5968bf276a96df0e9e
Brian Bugert <coolbots7@gmail.com>
2018-12-08 14:06:48 -0800

    Create chromosome object

---
+++ GeneticAlgorithm/chromosome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    public class chromosome
+    {
+        public List<int> gene { get; set; }
+        public double score { get; set; }
+
+        public chromosome()
+        {
+            gene = new List<int>();
+            score = double.MinValue;
+        }
+    }
+}
